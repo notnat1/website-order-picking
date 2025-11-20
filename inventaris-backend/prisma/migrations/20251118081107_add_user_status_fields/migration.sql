18f743efef6616efc45663e85e3d640322f6bbf6
notnat1 <natteishere11@gmail.com>
2025-11-20 10:57:57 +0700

    Fix: Correct case sensitivity in Prisma migration files

--- inventaris-backend/prisma/migrations/20251118081107_add_user_status_fields/migration.sql
+++ inventaris-backend/prisma/migrations/20251118081107_add_user_status_fields/migration.sql
@@ -1,10 +1,10 @@
 /*
   Warnings:
 
-  - You are about to drop the column `status` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `status` on the `User` table. All the data in the column will be lost.
 
 */
 -- AlterTable
-ALTER TABLE `user` DROP COLUMN `status`,
+ALTER TABLE `User` DROP COLUMN `status`,
     ADD COLUMN `deactivatedAt` DATETIME(3) NULL,
     ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true;
